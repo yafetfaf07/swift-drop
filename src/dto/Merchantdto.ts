2af01b008e74a5acc85b9413338b0ea0751c866c
Yafet Yosef <japhetkumalo23@gmail.com>
2025-04-11 21:44:46 +0300

    Added Merchant DTO

---
+++ src/dto/Merchantdto.ts
@@ -0,0 +1,26 @@
+import { Schema } from 'mongoose';
+
+// This is for creating a new merchant
+export interface MerchantDTO {
+  id: Schema.Types.ObjectId;
+  firstname: string;
+  lastname: string;
+  phone_no: string;
+  password: string;
+  address: [number];
+}
+
+// This is for changing profile
+export interface ProfileChangeDTO {
+    id:Schema.Types.ObjectId,
+    firstname:string,
+    lastname:string,
+    phone_no:string,
+    address:[number]
+}
+
+// This is for changing password
+export interface PasswordChangeDTO {
+    id:Schema.Types.ObjectId,
+    password:string
+}
