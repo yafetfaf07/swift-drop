483632cb300921e22272b1601e0ee3e4920c6ef7
Yafet Yosef <japhetkumalo23@gmail.com>
2025-04-15 10:10:02 +0300

    Added middleware for supporting form-data

---
+++ src/middlewares/multerconfig.ts
@@ -0,0 +1,25 @@
+// src/middleware/multerConfig.ts
+import multer from 'multer';
+import path from 'path';
+import fs from 'fs';
+
+const uploadDir = path.join(process.cwd(), 'uploads');
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    const uniqueName = `${Date.now()}-${file.originalname}`;
+    cb(null, uniqueName);
+  }
+});
+
+export const upload = multer({
+  storage,
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
