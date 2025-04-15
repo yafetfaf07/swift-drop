d3a25f23c458a029f102a5973922636cbeaa8029
Yafet Yosef <japhetkumalo23@gmail.com>
2025-04-15 12:45:53 +0300

    Changed Schema Id to mongoose

--- src/dto/Productdto.ts
+++ src/dto/Productdto.ts
@@ -1,7 +1,7 @@
-import { Schema } from "mongoose";
+import mongoose from "mongoose";
 // this is for displaying purposes
 export interface ProductDTO {
-    id:Schema.Types.ObjectId,
+    id:mongoose.Types.ObjectId,
     name:string,
     category:string,
     price:number,
@@ -9,14 +9,15 @@ export interface ProductDTO {
 }
 // This is for uploading product stocks
 export interface ProductDetailDTO {
-    id:Schema.Types.ObjectId,
+    id:mongoose.Types.ObjectId,
     name:string,
     category:string,
     price:number,
     quantity:number,
     imageUrl:string,
+    mid:mongoose.Types.ObjectId
 }
 
 export interface ProductId {
-    id:Schema.Types.ObjectId
+    id:mongoose.Types.ObjectId
 }
